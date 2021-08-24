15ea8bfb280cc2eeccd4b4806987b03d13e44986
guno517 <60571418+guno517@users.noreply.github.com>
2021-08-25 00:16:52 +0900

    create button & css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faPaw, faTimes } from '@fortawesome/free-solid-svg-icons';
-
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            {/*화면크키 변환시 버튼 UI 변경*/ }
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}> {/*로고 클릭시 모바일 메뉴 버튼 사라지면서 홈으로 가기*/}
                         MARIO <FontAwesomeIcon icon={faPaw} />
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
@@ -27,21 +44,22 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/who' className='nav-links' onCLick={closeMobileMenu}>
-                                Who am I
+                            <Link to='/services' className='nav-links' onCLick={closeMobileMenu}>
+                                Services
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/projects' className='nav-links' onCLick={closeMobileMenu}>
-                                Projects
+                            <Link to='/products' className='nav-links' onCLick={closeMobileMenu}>
+                                Products
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/about' className='nav-links-mobile' onCLick={closeMobileMenu}>
-                                About
+                            <Link to='/sign-up' className='nav-links-mobile' onCLick={closeMobileMenu}>
+                                Sign up
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
                 </div>
             </nav>
         </>
