84d76b63d7f74b2a9672c728565e42bab061d7e8
guno517 <60571418+guno517@users.noreply.github.com>
2021-08-26 03:11:23 +0900

    footer 추가

---
+++ src/components/Footer.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import {Button} from './Button'
+import {Link} from 'react-router-dom'
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaw } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Join the Advanture newletter to receive our best vacation deals
+                </p>
+                <p className="footer-subscription-text">
+                    You can unsubscribe at any time.
+                </p>
+                <div className="input-areas">
+                    <form>
+                        <input type="email" name="email" placeholder="your Email" className="footer-input" />
+                    </form>
+                    <Button buttonStyle='btn--outline'>Subscribe</Button>
+                </div>
+            </section>
+            <div className="footer-links">
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to= '/sign-up'>How it works</Link>
+                        <Link to= '/'>Testimonials</Link>
+                        <Link to= '/'>Careers</Link>
+                        <Link to= '/'>Investors</Link>
+                        <Link to= '/'>Terms of Service</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>Contact Us</h2>
+                        <Link to= '/'>name</Link>
+                        <Link to= '/'>email</Link>
+                        <Link to= '/'>Phone</Link>
+                        <Link to= '/'>Address</Link>
+                    </div>
+                </div>
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>Videos</h2>
+                        <Link to= '/'>Submit Video</Link>
+                        <Link to= '/'>Ambassadors</Link>
+                        <Link to= '/'>Agency</Link>
+                        <Link to= '/'>Influencer</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>Social Media</h2>
+                        <Link to= '/'>Instagram</Link>
+                        <Link to= '/'>Facebook</Link>
+                        <Link to= '/'>Youtube</Link>
+                        <Link to= '/'>Twitter</Link>
+                    </div>
+                </div>
+            </div>
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="footer-logo">
+                        <Link to='/' className="social-logo">
+                            MARIO <FontAwesomeIcon className='fapaw' icon={faPaw} />
+                        </Link>
+                    </div>
+                    <small className="website-rights">MARIO 2021</small>
+                    <div className="social-icons">
+                        <Link className="social-icon-link facebook" 
+                        to='/' 
+                        target='_blank' 
+                        aria-label='Facebook'>
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </Link>
+                    </div>
+                    <div className="social-icons">
+                        <Link className="social-icon-link instagram" 
+                        to='/' 
+                        target='_blank' 
+                        aria-label='Instagram'>
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </Link>
+                    </div>
+                    <div className="social-icons">
+                        <Link className="social-icon-link twitter" 
+                        to='/' 
+                        target='_blank' 
+                        aria-label='Twitter'>
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Footer
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faPaw, faTimes } from '@fortawesome/free-solid-svg-icons';
-import './Navbar.css';
-import { Button } from './Button';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faPaw, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+import "./Navbar.css";
+import { Button } from "./Button";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            {/*화면크키 변환시 버튼 UI 변경*/ }
-            setButton(false);
-        } else {
-            setButton(true);
-        }
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      {
+        /*화면크키 변환시 버튼 UI 변경*/
+      }
+      setButton(false);
+    } else {
+      setButton(true);
     }
+  };
 
-    useEffect(() => {
-        showButton()
-    }, [])
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}> {/*로고 클릭시 모바일 메뉴 버튼 사라지면서 홈으로 가기*/}
-                        MARIO <FontAwesomeIcon icon={faPaw} />
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <FontAwesomeIcon icon={click ? faTimes : faBars} /> {/*클릭시 값에 따라 아이콘 변경*/}
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className="nav-item">
-                            <Link to='/' className='nav-links' onCLick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/services' className='nav-links' onCLick={closeMobileMenu}>
-                                Services
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/products' className='nav-links' onCLick={closeMobileMenu}>
-                                Products
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to='/sign-up' className='nav-links-mobile' onCLick={closeMobileMenu}>
-                                Sign up
-                            </Link>
-                        </li>
-                    </ul>
-                    {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            {" "}
+            {/*로고 클릭시 모바일 메뉴 버튼 사라지면서 홈으로 가기*/}
+            MARIO <FontAwesomeIcon icon={faPaw} />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <FontAwesomeIcon icon={click ? faTimes : faBars} />{" "}
+            {/*클릭시 값에 따라 아이콘 변경*/}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onCLick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onCLick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onCLick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onCLick={closeMobileMenu}
+              >
+                Sign up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
